ee9050d3c28e865a3675d9e85797033eb9e2c83b
Vova Space <vova.space@icloud.com>
2019-05-19 17:26:38 +0300

    Change: resources loading

--- src/index.jsx
+++ src/index.jsx
@@ -4,11 +4,4 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './components/App/App';
 
-function importAll(r) {
-  r.keys().forEach(r);
-}
-
-// Load resources
-importAll(require.context('./resources', true, /.*\.*$/));
-
 ReactDOM.render(<App />, document.getElementById('root'));
